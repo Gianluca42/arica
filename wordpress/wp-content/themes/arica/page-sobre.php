848699a2c10a23f4512319a06761ef29adf0edd7
Gianluca Fiorini <gianluca1997@gmail.com>
2021-03-05 13:13:41 -0300

    sobre css html

--- wordpress/wp-content/themes/arica/page-sobre.php
+++ wordpress/wp-content/themes/arica/page-sobre.php
@@ -44,7 +44,7 @@ get_header();
         <h2>Nome do projeto</h2>
     </div>
   </div>
-  <div class="sobre-box">
+  <div class="sobre-box grande">
     <p>Está em processo de pós-produção dos documentários Vira-Latas - escrito e dirigido por Daniel Torres e James Salinas - e Urubu, com direção de Daniel Torres e Paulo Oliveira, ambos com previsão de lançamento para o segundo semestre de 2021. </p>
     <p>A Arica Filmes está também em processo de desenvolvimento dos filmes, Solidão Também Acompanha, com roteiro de James Salinas, e O Morto do Pântano, dirigido por Claudio Ellovitch. </p>
     <p>	É a idealizadora organizadora da mostra de cinema independente Cinema na Santa, que exibe filmes e realiza bate-papos com diretores e realizadores. Com mais de 35° edições, o Cinema na Santa se consolida a cada sessão, por meio de um público diversificado que busca difundir a concepção do cinema com discussões ricas e conteúdo sobre o universo da sétima arte. </p>
@@ -55,8 +55,8 @@ get_header();
   <h1>os sócios</h1>
   <div class="bloco-socios">
     <div class="fotos-sobre">
-      <img src="<?php bloginfo('template_url'); ?>/assets/imgs/sociosimg.svg" alt="">
-      <img src="<?php bloginfo('template_url'); ?>/assets/imgs/sociosimg.svg" alt="">
+      <img src="<?php bloginfo('template_url'); ?>/assets/imgs/sociosimg.png" alt="">
+      <img src="<?php bloginfo('template_url'); ?>/assets/imgs/sociosimg.png" alt="">
     </div>
     <div class="texto-socio">
       <h1>Daniel Torres</h1>
@@ -65,14 +65,15 @@ get_header();
     </div>
   </div>
   <div class="bloco-socios">
-    <div class="fotos-sobre">
-      <img src="<?php bloginfo('template_url'); ?>/assets/imgs/sociosimg.svg" alt="">
-      <img src="<?php bloginfo('template_url'); ?>/assets/imgs/sociosimg.svg" alt="">
-    </div>
+
     <div class="texto-socio">
       <h1>Daniel Torres</h1>
       <p>Daniel Torres é Produtor Executivo no filme O Morto do Pântano. É sócio da Arica Filmes, onde realiza trabalhos de direção de produção e produção executiva. Começou sua carreira em 2000, como ator. Está à frente do desenvolvimento e produção executiva do longa metragem Solidão Também Acompanha e direção e roteiro do documentário Vira-latas (ambos em andamento). Foi diretor e produtor executivo do piloto da série Luz e Sombra no Mofo (2014); diretor do curta-metragem Passagem das Horas (2018) e diretor, roteirista e produtor executivo da série documental Quintal Musical (2018 - 2020), entre outros trabalhos. O profissional também apresentou e organizou eventos de cinema, como Festival Art'Deco, Sessão Gata Cine, Cine Guerrilha e Triângulo.</p>
       <p>Como ator, esteve no elenco de filmes como Dois Coelhos (2012);  Colegas (2013) Tudo que Aprendemos Juntos (2015); Corpo Elétrico (2017) e Mais Forte que o Mundo (2016). Participou também de séries como 9mm São Paulo (2008); Pacto de Sangue (2018); Cidades Invisíveis (2019) e Irmandade (2019). Como ator, participou também de diversos curtas metragens e filmes publicitários.</p>
+    </div>
+    <div class="fotos-sobre">
+      <img src="<?php bloginfo('template_url'); ?>/assets/imgs/sociosimg.png" alt="">
+      <img src="<?php bloginfo('template_url'); ?>/assets/imgs/sociosimg.png" alt="">
     </div>
   </div>
 </section>
